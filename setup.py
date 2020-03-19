3b2a959cb91724c21ae32813fc910db66d0e5e66
smthnspcl <pascal@eberlein.io>
2020-03-19 10:26:19 +0100

    update

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     keywords="duckduckgo python search query",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines()
 )
